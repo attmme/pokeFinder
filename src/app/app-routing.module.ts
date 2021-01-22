1ecf008d0c10e3fe9e6b3b9d5657d62807803f48
Oscar <ogomezsts51l@gmail.com>
2021-01-22 17:51:53 +0100

    save

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '**', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
   { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterPageModule) },
   { path: 'perfil', loadChildren: () => import('./perfil/perfil.module').then(m => m.PerfilPageModule) },
   { path: 'buscador', loadChildren: () => import('./buscador/buscador.module').then(m => m.BuscadorPageModule) },
   { path: 'qr', loadChildren: () => import('./modal-qr-scanner/modal-qr-scanner.module').then(m => m.ModalQrScannerPageModule) },
+  { path: '**', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
 
 ];
 @NgModule({
