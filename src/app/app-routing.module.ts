c03c9375ba77f13d65ca6e9c90e505303d281931
Oscar <ogomezsts51l@gmail.com>
2021-01-22 17:50:11 +0100

    save

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,35 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'register',
-    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'perfil',
-    loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilPageModule)
-  },
-  {
-    path: 'buscador',
-    loadChildren: () => import('./buscador/buscador.module').then( m => m.BuscadorPageModule)
-  },
-  {
-    path: 'qr',
-    loadChildren: () => import('./modal-qr-scanner/modal-qr-scanner.module').then( m => m.ModalQrScannerPageModule)
-  },
-
+  { path: '**', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
+  { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterPageModule) },
+  { path: 'perfil', loadChildren: () => import('./perfil/perfil.module').then(m => m.PerfilPageModule) },
+  { path: 'buscador', loadChildren: () => import('./buscador/buscador.module').then(m => m.BuscadorPageModule) },
+  { path: 'qr', loadChildren: () => import('./modal-qr-scanner/modal-qr-scanner.module').then(m => m.ModalQrScannerPageModule) },
 
 ];
 @NgModule({
@@ -39,4 +16,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
