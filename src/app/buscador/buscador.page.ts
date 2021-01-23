f50099ede84ffefe3951dd1368ca751563a06fa8
victorgg1993 <vigomga1993@gmail.com>
2021-01-23 18:28:52 +0100

    treballant amb el slider d'imatges

--- src/app/buscador/buscador.page.ts
+++ src/app/buscador/buscador.page.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 
 import { ModalController } from '@ionic/angular';
 import { PerfilPage } from '../perfil/perfil.page';
 
 import { Router } from '@angular/router';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-buscador',
@@ -13,11 +14,47 @@ import { Router } from '@angular/router';
 })
 export class BuscadorPage implements OnInit {
 
+  @ViewChild('slideWithNav', { static: false }) slideWithNav: IonSlides;
+
+  sliderOne: any;
+
+  //Configuration for each Slider
+  slideOptsOne = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    autoplay: false
+  };
+
   constructor(
     private _router: Router,
     private menu: MenuController,
     public modalController: ModalController
-  ) { }
+  ) {
+    
+    //Item object for Nature
+    this.sliderOne =
+    {
+      isBeginningSlide: true,
+      isEndSlide: false,
+      slidesItems: [
+        {
+          id: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/145.png'
+        },
+        {
+          id: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/27.png'
+        },
+        {
+          id: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/60.png'
+        },
+        {
+          id: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/95.png'
+        },
+        {
+          id: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png'
+        }
+      ]
+    };
+  }
 
   ngOnInit() {
   }
@@ -46,5 +83,45 @@ export class BuscadorPage implements OnInit {
 
     return await modal.present();
   }
+
+
+  // ------------------------------------------------------------------ <tmp>
+  //Move to Next slide
+  slideNext(object, slideView) {
+    slideView.slideNext(500).then(() => {
+      this.checkIfNavDisabled(object, slideView);
+    });
+  }
+
+  //Move to previous slide
+  slidePrev(object, slideView) {
+    slideView.slidePrev(500).then(() => {
+      this.checkIfNavDisabled(object, slideView);
+    });;
+  }
+
+  //Method called when slide is changed by drag or navigation
+  SlideDidChange(object, slideView) {
+    this.checkIfNavDisabled(object, slideView);
+  }
+
+  //Call methods to check if slide is first or last to enable disbale navigation  
+  checkIfNavDisabled(object, slideView) {
+    this.checkisBeginning(object, slideView);
+    this.checkisEnd(object, slideView);
+  }
+
+  checkisBeginning(object, slideView) {
+    slideView.isBeginning().then((istrue) => {
+      object.isBeginningSlide = istrue;
+    });
+  }
+
+  checkisEnd(object, slideView) {
+    slideView.isEnd().then((istrue) => {
+      object.isEndSlide = istrue;
+    });
+  }
+  // ------------------------------------------------------------------ </tmp>
 
 }
